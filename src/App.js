f92f896964f77f50be6d28bd64b3507b068f1267
Diwakar <diwakarraj1122@gmail.com>
2020-10-05 14:28:21 +0530

    start the  development of home screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomePage from './components/views/HomePage/HomePage';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Fast Fingers </h1>
+  
+      <HomePage/> 
     </div>
   );
 }
---
+++ src/components/views/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Home.css'
+import bg from '../../../assets/group-243.svg';
+// import path from '../../../assets';
+
+ 
+ const HomePage = () => {
+    return (  
+        <div  class="Group-243">
+         
+      <p>Hello</p>
+        
+
+       
+        </div> );
+}
+ 
+
+export default HomePage;
