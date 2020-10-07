fadd00449dc12ba82a71f9824bbcd715a75135e8
AvijeetDey96 <avijeetdey96@gmail.com>
2020-10-07 23:34:51 +0530

    Home screen

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import HomePage from './components/views/HomePage/HomePage';
 function App() {
   return (
     <div className="App">
-  
       <HomePage/> 
     </div>
   );
 }
 
 export default App;
+ 
--- src/components/views/HomePage/HomePage.js
+++ src/components/views/HomePage/HomePage.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Home.css'
-import bg from '../../../assets/group-243.svg';
-// import path from '../../../assets';
-
- 
- const HomePage = () => {
-    return (  
-        <div  class="Group-243">
-         
-      <p>Hello</p>
-        
-
-       
-        </div> );
+import keyboard from "../../../assets/keyboard.svg";
+import play from "../../../assets/Icon-awesome-play.svg";
+import useHomeState from '../../../hooks/useHomeState.js'
+const HomePage = () => {
+const [value,handleChange,reset] = useHomeState("")
+const data =(v) =>{
+  console.log('daat',v);
 }
- 
+  return (<div className="Group-243" >
+    <div className="container-fluid" >
+      <div className="row; fast-div"><img src={keyboard} alt="" className="Icon-awesome-keyboard" /></div>
+      <div className="row; fast-div"><span className="fast-fingers"> Fast Fingers</span></div>
+      <div className="row; fast-div"><p className=" the-ultimate-typing-game">The ultimate typing game</p></div>
+      <form onSubmit={handleChange}>
+      <div className="row;fast-div"><input onChange={data(value)}  placeholder="TYPE YOUR NAME" id="name" value={value.name} required  className="Rectangle-2" /></div>
+      <div className="row;fast-div">
+        <select name="mode" value={value.mode} required id="mode" className="Rectangle-5">
+          <option className="Rectangle-6" value="Easy">Easy</option>
+          <option className="Rectangle-6" value="Medium">Medium</option>
+          <option className="Rectangle-6" value="Hard">Hard</option>
+        </select> </div>
+        <div className="row; fast-div" type="submit" >  <p className="start-game"> <img src={play} alt="" className="Icon-awesome-play" />START GAME</p> </div>
+        </form>
+    </div>
+  </div>);
+}
+
 
 export default HomePage;
