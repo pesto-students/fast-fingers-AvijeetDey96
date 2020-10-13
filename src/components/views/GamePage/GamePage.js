238d70e41aa201f706be3b73706aa2309c6aea17
AvijeetDey96 <avijeetdey96@gmail.com>
2020-10-13 17:34:30 +0530

    make random input

--- src/components/views/GamePage/GamePage.js
+++ src/components/views/GamePage/GamePage.js
@@ -4,17 +4,36 @@ import './Game.css'
 import person from "../../../assets/material-person.svg";
 import joyStick from "../../../assets/awesome-gamepad.svg";
 import timer from "../../../assets/timer.svg";
-
+import cross from "../../../assets/metro-cross.svg";
+import home from "../../../assets/awesome-home.svg";
+import dictionary from "../../../data/dictionary.json"
 class GamePage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-
+    this.state = {
+      word:'',
+      typedName:''
+    };
+this.getRandomArbitrary = this.getRandomArbitrary.bind(this);
+this.compareWord = this.compareWord.bind(this)
+  }
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
   }
   componentDidMount() {
-
+    var count = Object.keys(dictionary).length;
+     let number = Math.round(this.getRandomArbitrary(1,count));
+     this.setState({
+      word:dictionary[number]
+    })
+  }
+  compareWord(evt){
+console.log('evt',evt)
   }
   render() {
+    const { word,typedName } = this.state;
+    const Timer =() =>{
+    }
     const data = JSON.parse(localStorage.getItem('userData'))
 
     return (
@@ -59,14 +78,14 @@ class GamePage extends Component {
               <div className="row">
                 <div className="col-4"></div>
 
-                <div className="col-4"><img src={timer} className="Group-249" /></div>
+                <div className="col-4"><img src={timer} /><span className="Group-249" ><p className="countdown">2:14</p> </span > </div>
                 <div className="col-4"></div>
-                <div className="col-12"><p className="window">window</p> </div>
-                <div className="col-12"><input className="Rectangle-9" type="text"/> </div>
+    <div className="col-12"><p className="window">{word}</p> </div>
+                <div className="col-12"><input name="words" onChange={this.compareWord}  value={this.state.typedName}  className="Rectangle-9" type="text"/> </div>
               </div>
 
             </div>
-
+  <div className="col-12" ><img className="cross-image" src={cross} /> <p className="stop-game">STOP GAME</p> <img className="home-image" src={home} /> </div>
           </div>
 
         </div>
